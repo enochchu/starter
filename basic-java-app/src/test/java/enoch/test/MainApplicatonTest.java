77a421945098c573aa5ff0ad1e32a55134b7bbf0
Enoch Chu <git@enochchu.com>
2018-11-05 22:56:28 -0800

    basic-java-app - Initial Commit

---
+++ basic-java-app/src/test/java/enoch/test/MainApplicatonTest.java
@@ -0,0 +1,15 @@
+package enoch.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MainApplicatonTest {
+
+    @Test
+    public void shouldBeAbleToRunTest() {
+        // This test is made to fail on purposes.
+        assertEquals(1,2);
+    }
+
+}
