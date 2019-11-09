55c70f3bcdbd9927e17b4d855442dab5aa7d3a61
Enoch Chu <git@enochchu.com>
2019-11-08 23:58:13 -0800

    basic-typescript-workspace

---
+++ basic-typescript-workspace/tsconfig.ts
@@ -0,0 +1,12 @@
+{
+    "compilerOptions" : {
+        "lib": ["es6", "dom"],
+
+        "module": "es6",
+        "sourceMap": true,
+        "outFile": "./",
+
+        "strict": true,
+        "alwaysStrict": true
+    }
+}
