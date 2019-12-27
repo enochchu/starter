62a259b27109345195beb5e22a0fe2413d97c48b
Enoch Chu <git@enochchu.com>
2019-12-26 22:38:53 -0800

    basic-javafx-app - Initial Commit

---
+++ basic-javafx-app/src/main/java/enoch/MainApplication.java
@@ -0,0 +1,14 @@
+package enoch;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World");
+
+        primaryStage.show();
+    }
+}
