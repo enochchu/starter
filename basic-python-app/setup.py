eff6764e4617afb176888995171a025f2185543b
Enoch Chu <git@enochchu.com>
2020-10-17 02:49:31 -0700

    setup.py - update

--- basic-python-app/setup.py
+++ basic-python-app/setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
-
 import os
 import sys
+
+VENV_FOLDER = 'venv'
 
 NAME = 'basic-python-app'
 DESCRIPTION = 'Just a basic python app'
@@ -11,26 +12,77 @@ AUTHOR = 'Enoch Chu'
 REQUIRES_PYTHON = '>=3.6.0'
 VERSION = '0.1.0'
 
-# Setup
-if sys.argv[-1] == 'initial-setup':
-    os.system('python -m virtualenv venv')
-    print('Remember to pip install wheel if you want to build this package!')
+# Packages you want your build to export
+local_install = []
+
+packages = [
+   'enoch'
+]
+
+# Required dependencies
+required = [ ]
+
+test_requirements = [
+   'pytest',
+   'nose'
+]
+
+def remove_venv():
+    print('Remove venv')
+    os.system(f"rm -R { VENV_FOLDER } ")
+
+def setup_venv():
+    print('Setting up virtualenv')
+    os.system(f"python -m virtualenv { VENV_FOLDER }")
+    os.system(f"{ os.path.abspath(f'./{VENV_FOLDER}/Scripts/python') } -m pip install --upgrade pip")
+
+def setup_build_system():
+    print('Installing wheel')
+    os.system(f"{ os.path.abspath(f'./venv/Scripts/pip') } install wheel")
+
+def setup_local_dependencies():
+    print('Installing local dependencies')
+    for package in local_install:
+        os.system(f"{os.path.abspath(f'./venv/Scripts/pip')} install { os.path.abspath(package) }")
+
+def setup_dependencies():
+    print('Installing required dependencies')
+    for package in required:
+        os.system(f"{os.path.abspath(f'./venv/Scripts/pip')} install { package }")
+
+def setup_test_dependencies():
+    print('Installing test dependencies')
+    for package in test_requirements:
+        os.system(f"{os.path.abspath(f'./venv/Scripts/pip')} install { package }")
+
+def build_all():
+    setup_venv()
+    setup_build_system()
+    setup_local_dependencies()
+    setup_dependencies()
+    setup_test_dependencies()
+
+## Setup
+if sys.argv[-1] == 'setup':
+    build_all()
     sys.exit()
 
-# Build
+## Setup
+if sys.argv[-1] == 'install-dependencies':
+    setup_local_dependencies()
+    setup_dependencies()
+    setup_test_dependencies()
+    sys.exit()
+
+## Clean
+if sys.argv[-1] == 'clean':
+    remove_venv()
+    sys.exit()
+
+## Build
 if sys.argv[-1] == 'build':
     os.system('python setup.py sdist bdist_wheel')
     sys.exit()
-
-packages = ['enoch']
-
-install_requires = []
-
-test_required = [
-    'coverage',
-    'pytest',
-    'pytest-watch'
-]
 
 setup(
     name=NAME,
@@ -41,7 +93,6 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=packages,
-    install_requires=install_requires,
-    test_required=test_required,
-    license='MIT'
+    install_requires= required,
+    tests_require = test_requirements
 )
